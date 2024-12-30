74336818b8ad1835912b743947769d7790fac41d
YupaLorkan <yupa.lorkanchanakul@accenture.com>
2024-12-30 16:47:05 +0700

    Update streamlit_app.py
    Update

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,5 @@
 # Import python packages
 import streamlit as st
-from snowflake.snowpark.functions import col
 
 # Write directly to the app
 st.title(":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
@@ -11,6 +10,8 @@ st.write(
 
 name_on_order = st.text_input('Name on Smoothie:')
 st.write('The name on your Smoothie will be:', name_on_order)
+
+from snowflake.snowpark.functions import col
 
 cnx = st.connection("snowflake")
 session = cnx.session()
